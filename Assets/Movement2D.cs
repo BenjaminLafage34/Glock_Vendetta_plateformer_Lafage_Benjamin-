759318fd023134e3a507713b9e162d885d47ba2f
99aa66 <benjamin.lafage31@gmail.com>
2023-04-05 17:00:04 +0200

    push 2 05/04

--- Assets/Movement2D.cs
+++ Assets/Movement2D.cs
@@ -8,10 +8,11 @@ using static UnityEngine.GraphicsBuffer;
 public class Movement2D : MonoBehaviour
 {
     float horizontalValue;
+    float verticalValue;
     Rigidbody2D rb;
-    Vector3 m_Velocity = Vector3.zero; 
+    Vector3 m_Velocity = Vector3.zero;
     [SerializeField] private float m_MovementSmoothing = .05f;
-    float speed = 40f;
+    float speed = 30f;
     private float m_JumpForce = 800f;
     private bool jumping = false;
     public GameObject pivotGun;
@@ -20,13 +21,14 @@ public class Movement2D : MonoBehaviour
     public SpriteRenderer bras;
     public int JumpCount;
     private bool canDash = true;
-    private float dashingPower = 24f;
+    private float dashingPower = 15f;
     private bool isDashing;
-    private float dashingTime = 0.2f;
-    private float dashingCooldaown = 1f;
+    private float dashingTime = 0.4f;
+    private float dashingCooldown = 2f;
+    
 
-    
-    
+    [SerializeField] private TrailRenderer tr;
+
 
     SpriteRenderer sr;
 
@@ -40,16 +42,29 @@ public class Movement2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(rb.velocity.y == 0)
+        /*
+        horizontalValue = Input.GetAxis("Horizontal");
+        verticalValue = Input.GetAxis("Vertical");
 
-       {
+        if (horizontalValue > 0) sr.flipX = false;
+        else if (horizontalValue < 0) sr.flipX = true;
+        */
+        if (isDashing)
+        {
+            return;
+        }
+        if (rb.velocity.y == 0)
+
+        {
 
             JumpCount = 0;
-            
-       
+
+
         }
+      
+
         horizontalValue = Input.GetAxis("Horizontal");
-        if (Input.GetButtonDown("Jump") && jumping==false && JumpCount<2)
+        if (Input.GetButtonDown("Jump") && jumping == false && JumpCount < 2)
         {
             jumping = true;
 
@@ -58,54 +73,71 @@ public class Movement2D : MonoBehaviour
         Vector3 v_diff = (target - transform.position).normalized;
         float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
         pivotGun.transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg);
-        
+
         if (pivotGun.transform.rotation.eulerAngles.z > 90 && pivotGun.transform.rotation.eulerAngles.z < 270)
         {
-            bras.flipY=true;
-            
+            bras.flipY = true;
+
             spawnPoint.GetComponent<Transform>().localPosition = new Vector2(1.9f, -0.35f);
         }
         else
         {
             bras.flipY = false;
-            
+
             spawnPoint.GetComponent<Transform>().localPosition = new Vector2(1.9f, 0.3f);
         }
-        
-       //Debug.Log(v_diff.x);
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        //Debug.Log(v_diff.x);
+
         sr.flipX = (v_diff.x < 0);
         if (Input.GetButtonDown("Fire1"))
         {
-            
-            Instantiate(bulletRef,spawnPoint.transform.position, pivotGun.transform.rotation);
+
+            Instantiate(bulletRef, spawnPoint.transform.position, pivotGun.transform.rotation);
         }
 
-      
+
     }
-     /*private IEnumerator Dash()
+    private IEnumerator Dash()
     {
         canDash = false;
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
-        rb.velocity = new Vector2
-    }*/
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
+    }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         Vector3 targetVelocity = new Vector2(horizontalValue * 10f * speed * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
         if (jumping)
         {
             rb.AddForce(new Vector2(0f, m_JumpForce));
-            JumpCount ++;                    
+            JumpCount++;
             jumping = false;
         }
 
     }
-    
-       
-    }
-   
+
+
+}
+
+
 
