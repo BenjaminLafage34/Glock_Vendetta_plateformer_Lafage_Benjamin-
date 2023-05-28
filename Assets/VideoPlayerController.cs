2a57e1e16cae512718549228e6216ee2184cf970
BenjaminLafage34 <benjamin.lafage31@gmail.com>
2023-05-28 22:29:20 +0200

    ajout de la cinématique

---
+++ Assets/VideoPlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        // Récupérer le composant VideoPlayer attaché à ce GameObject
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        // S'abonner à l'événement de fin de lecture de la vidéo
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        // Charger la scène "Domaine"
+        SceneManager.LoadScene("Domaine");
+    }
+}
