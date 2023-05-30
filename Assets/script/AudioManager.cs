4d5b924bfbc13d301c6b92062637fd491b425e01
BenjaminLafage34 <benjamin.lafage31@gmail.com>
2023-05-30 18:21:20 +0200

    Ajout des anim carolina +boites de dialogue

--- Assets/script/AudioManager.cs
+++ Assets/script/AudioManager.cs
@@ -9,13 +9,22 @@ public class AudioManager : MonoBehaviour
     [Header("---------- Audio Clip----------")]
     public AudioClip background;
     public AudioClip revenge;
-
+    public AudioClip Boss;
    
     private void Start()
     {
         musicSource.clip = background;
         musicSource.Play();
     }
- 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Changer la musique ici
+            musicSource.clip = Boss;
+            musicSource.Play();
+        }
+    }
+
 }
 
